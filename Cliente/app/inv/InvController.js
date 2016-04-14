5919c7ccaa2876752348b2c70c6271922d73fbf5
anuarml <anuar_ml93@hotmail.com>
2016-04-14 14:09:08 -0500

    Inicio prueba bootstrap table
    Se empezo la tabla de inventarios con la libreria bootstrap table

--- Cliente/app/inv/InvController.js
+++ Cliente/app/inv/InvController.js
@@ -14,8 +14,8 @@ angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
 			});
 	}])
 
-	.controller('InvController', ['$scope','$http','$location','Inv','INV_ROUTES',
-		function($scope,$http,$location,Inv,INV_ROUTES){
+	.controller('InvController', ['$scope','$http','$location','Inv','INV_ROUTES','$compile',
+		function($scope,$http,$location,Inv,INV_ROUTES,$compile){
 
 		//var code = $routeParams.code;
 
@@ -25,20 +25,53 @@ angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
 			$scope.filters=[{Codigo:'',Descripcion:'',Fabricante:'',Empresa:''}]
 			$scope.inv = inv;
 			$scope.invSafe = angular.copy(inv);
-			$scope.itemsByPage=25;
+			$scope.itemsByPage=25;	
 			$scope.displayedPages=5;
 			//if(angular.isDefined(code)){
 			//$scope.newInv = angular.copy(Inv.get(code));
 			//$scope.isEdit = true;
 			//}
+			$('#invTable').bootstrapTable({
+				data: $scope.inv
+			});
+			//$scope.inv[0].Articulo = 'Hola';
+			//$('#invTable').bootstrapTable('refresh');
+
 		}, function(msg){
 			console.error(msg);
 		});
+
 
 		$scope.search = function(filters){
 			Inv.search(filters.Codigo,filters.Descripcion,filters.Fabricante,filters.Empresa);
 			//$location.path(ITEM_ROUTES.LIST);
 		}
 
+		$scope.loadData = function() {
+			$scope.inv = $scope.inv.slice(1,2);
+		}
+
 		$scope.lang = 'es';
-	}]);
+	}])
+
+	.directive('relinkEvent', function($rootScope) {
+	    return {
+	        transclude: 'element',
+	        restrict: 'A',
+	        link: function(scope, element, attr, ctrl, transclude) {
+	            var previousContent = null;
+	            var triggerRelink = function() {
+	                if (previousContent) {
+	                    previousContent.remove();
+	                    previousContent = null;
+	                }
+	                transclude(function (clone) {
+	                    element.parent().append(clone);
+	                    previousContent = clone;
+	                });
+	            };
+	            triggerRelink();                
+	            $rootScope.$on(attr.relinkEvent, triggerRelink);
+	        }
+	    };
+	});
