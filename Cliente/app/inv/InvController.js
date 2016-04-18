3830e1e833e80f3b29122facdd2523a02c53452f
anuarml <anuar_ml93@hotmail.com>
2016-04-18 11:29:04 -0500

    Inicio Tabla whenzinxin
    Se inicio la vista y el controlador para añadir la boostrap table a la
    vista del inventario

--- Cliente/app/inv/InvController.js
+++ Cliente/app/inv/InvController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
+angular.module('InvApp.inv', ['ngRoute',/*,'smart-table','lrDragNDrop',*/'bsTable'])
 
 	.constant('INV_ROUTES',{
 		'LIST':'/inv/list'
@@ -9,7 +9,7 @@ angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
 	.config(['$routeProvider','INV_ROUTES',function($routeProvider,INV_ROUTES) {
 		$routeProvider
 			.when(INV_ROUTES.LIST, {
-				templateUrl:'inv/partials/inv-list.html',
+				templateUrl:'inv/partials/index.html',
 				controller:'InvController'
 			});
 	}])
@@ -18,12 +18,109 @@ angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
 		function($scope,$http,$location,Inv,INV_ROUTES,$compile){
 
 		//var code = $routeParams.code;
+		$scope.workspaces = [];
+	    $scope.workspaces.push({ name: 'Workspace 1' });
+
+	    $scope.changeCurrentWorkspace = function (wk) {
+	        $scope.currentWorkspace = wk;
+	    };
 
 		Inv.all().then(function(inv){
-			$scope.dataBases=['Alumayab','Argentum','Herramax','La Viga','Alumik','Valsi','Mayalum','Alpina']
+			$scope.inv = inv;
+			//$scope.refresh();
+			/*$scope.$apply(function(){
+        		$scope.inv = inv;
+            });*/
+
+            $scope.workspaces[0].bsTableControl.options = {
+	                data: $scope.inv,
+	                rowStyle: function (row, index) {
+	                    return { classes: 'none' };
+	                },
+	                cache: false,
+	                height: 600,
+	                striped: true,
+	                pagination: true,
+	                pageSize: 10,
+	                pageList: [5, 10, 25, 50, 100, 200],
+	                search: true,
+	                showColumns: true,
+	                showRefresh: true,
+	                minimumCountColumns: 2,
+	                clickToSelect: false,
+	                showToggle: true,
+	                maintainSelected: true,
+	                columns: [{
+	                    field: 'Articulo',
+	                    title: 'Articulo',
+	                    align: 'right',
+	                    valign: 'bottom',
+	                    sortable: true
+	                }, {
+	                    field: 'Disponible',
+	                    title: 'Disponible',
+	                    align: 'center',
+	                    valign: 'bottom',
+	                    sortable: true
+	                }, {
+	                    field: 'Rama',
+	                    title: 'Rama',
+	                    align: 'center',
+	                    valign: 'middle',
+	                    sortable: true
+	                }, {
+	                    field: 'Descripcion',
+	                    title: 'Descripcion',
+	                    align: 'left',
+	                    valign: 'top',
+	                    sortable: true
+	                }, {
+	                    field: 'Grupo',
+	                    title: 'Grupo',
+	                    align: 'center',
+	                    valign: 'middle',
+	                    clickToSelect: false,
+	                    //formatter: flagFormatter,
+	                    // events: flagEvents
+	                }]
+	            };
+            /*if (!$scope.$$phase) $scope.$apply(function(){
+	          		var invT = [{
+						"Articulo":"001",
+						"Almacen":"Centro",
+						"Disponible":50,
+						"Rama":"arbol",
+						"Descripcion":"Mouse Razer R2500",
+						"Grupo":"LODEMORED",
+						"Categoria":"Computacion",
+						"Familia":"Accesorios",
+						"Linea":"Blanca",
+						"Fabricante":"DELL"
+					}]
+	          		$scope.inv = invT;
+	              });*/
+
+            /*setTimeout(function(){
+               $scope.$apply(function(){
+	          		var invT = [{
+						"Articulo":"001",
+						"Almacen":"Centro",
+						"Disponible":50,
+						"Rama":"arbol",
+						"Descripcion":"Mouse Razer R2500",
+						"Grupo":"LODEMORED",
+						"Categoria":"Computacion",
+						"Familia":"Accesorios",
+						"Linea":"Blanca",
+						"Fabricante":"DELL"
+					}]
+	          		$scope.inv = invT;
+	              });
+	        },100);*/
+			/*$scope.dataBases=['Alumayab','Argentum','Herramax','La Viga','Alumik','Valsi','Mayalum','Alpina']
 			$scope.columns=[{name:'Articulo',filter:false},{name:'Almacen',filter:false},{name:'Disponible',filter:false},{name:'Rama',filter:false},{name:'Descripcion',filter:false},{name:'Grupo',filter:true},{name:'Categoria',filter:false},{name:'Familia',filter:true},{name:'Linea',filter:true},{name:'Fabricante',filter:true}];
 			$scope.filters=[{Codigo:'',Descripcion:'',Fabricante:'',Empresa:''}]
-			$scope.inv = inv;
+			
 			$scope.invSafe = angular.copy(inv);
 			$scope.itemsByPage=25;	
 			$scope.displayedPages=5;
@@ -33,16 +130,79 @@ angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
 			//}
 			$('#invTable').bootstrapTable({
 				data: $scope.inv
-			});
+			});*/
 			//$scope.inv[0].Articulo = 'Hola';
 			//$('#invTable').bootstrapTable('refresh');
-
+			//$scope.$apply();
+			
 		}, function(msg){
 			console.error(msg);
 		});
 
+		$scope.workspaces.forEach(function (wk,index) {
+	        var colData = {workspace: wk.name};
+	        //wk.rows = makeRandomRows(colData);
+	        
+	        wk.bsTableControl = {
+	            options: {
+	                data: [],
+	                rowStyle: function (row, index) {
+	                    return { classes: 'none' };
+	                },
+	                cache: false,
+	                height: 600,
+	                striped: true,
+	                pagination: true,
+	                pageSize: 10,
+	                pageList: [5, 10, 25, 50, 100, 200],
+	                search: true,
+	                showColumns: true,
+	                showRefresh: true,
+	                minimumCountColumns: 2,
+	                clickToSelect: false,
+	                showToggle: true,
+	                maintainSelected: true,
+	                columns: [{
+	                    field: 'Articulo',
+	                    title: 'Articulo',
+	                    align: 'right',
+	                    valign: 'bottom',
+	                    sortable: true
+	                }, {
+	                    field: 'Disponible',
+	                    title: 'Disponible',
+	                    align: 'center',
+	                    valign: 'bottom',
+	                    sortable: true
+	                }, {
+	                    field: 'Rama',
+	                    title: 'Rama',
+	                    align: 'center',
+	                    valign: 'middle',
+	                    sortable: true
+	                }, {
+	                    field: 'Descripcion',
+	                    title: 'Descripcion',
+	                    align: 'left',
+	                    valign: 'top',
+	                    sortable: true
+	                }, {
+	                    field: 'Grupo',
+	                    title: 'Grupo',
+	                    align: 'center',
+	                    valign: 'middle',
+	                    clickToSelect: false,
+	                    //formatter: flagFormatter,
+	                    // events: flagEvents
+	                }]
+	            }
+	        };
+	        /*function flagFormatter(value, row, index) {
+	            return '<img src="' + row.flagImage + '"/>'
+	        }*/
 
-		$scope.search = function(filters){
+	    });
+		/*$scope.search = function(filters){
 			Inv.search(filters.Codigo,filters.Descripcion,filters.Fabricante,filters.Empresa);
 			//$location.path(ITEM_ROUTES.LIST);
 		}
@@ -51,7 +211,16 @@ angular.module('InvApp.inv', ['ngRoute','smart-table','lrDragNDrop'])
 			$scope.inv = $scope.inv.slice(1,2);
 		}
 
-		$scope.lang = 'es';
+		$scope.lang = 'es';*/
+
+		
+
+
+	    //Select the workspace in document ready event
+	    $(document).ready(function () {
+	        $scope.changeCurrentWorkspace($scope.workspaces[0]);
+	        //$scope.$apply();
+	    });
 	}])
 
 	.directive('relinkEvent', function($rootScope) {
